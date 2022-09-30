7b32341e57477d59a8c8bd08b9df4a3755e8c878
evgeniy <pavlinb4@gmail.com>
2022-09-30 12:28:11 +0300

    add option to clear terminal after 'N' answer

--- get_all_words_from_text/clear_text_with_spacy.py
+++ get_all_words_from_text/clear_text_with_spacy.py
@@ -1,6 +1,10 @@
 # https://spacy.io/usage
-import spacy
+import os
 import re
+
+import pyperclip
+import spacy
+from colorama import Fore
 
 
 def read_easy_words_file():
@@ -32,6 +36,39 @@ def clear_words_collection(lemmatised_set: set) -> list:
             cleared_words.append(word)
     return cleared_words
 
+def write_word_to_file(input_data:str):
+    with open('get_all_words_from_text/easy_words.txt', 'a') as text_file:
+        text_file.write(f'{input_data}\n')
+
+
+def add_word_to_file(input_data):
+    print(f"{Fore.BLUE}{input_data}{Fore.RESET}\nwill be ignore in future")
+    write_word_to_file(input_data)
+    # return input_data
+
+
+def make_choice(word):
+    pyperclip.copy(word)
+    choice = input(f"\nIs word {Fore.GREEN}{word}{Fore.RESET} easy or you want ignore it?\n{Fore.RED}Press Y{Fore.RESET} or "
+                   f"{Fore.RED}Press N{Fore.RESET}\n")
+
+    while choice.lower() != 'y' and choice.lower() != 'n':
+        choice = input(f"{Fore.GREEN}Please print{Fore.RESET}\n{Fore.RED}Y - to continue{Fore.RESET}\n"
+                       f"{Fore.RED}N - to stop{Fore.RESET}\n")
+    if choice.lower() == 'y':
+        add_word_to_file(word)
+    elif choice.lower() == 'n':
+        os.system('clear')
+        print(f'{Fore.GREEN} Lets go to next word{Fore.RESET}')
+
+
+
+def add_easy_word_to_ignore(clear_words:set,word_len:int):
+    for _ in clear_words:
+        if len(_) == word_len:
+            make_choice(_)
+
+
 
 if __name__ == '__main__':
     path_to_file = '/Volumes/big4photo/Downloads/Lamb to the Slaughter.txt'
@@ -40,6 +77,6 @@ if __name__ == '__main__':
     print(f'words in text = {len(words_from_text)}')
     clear_words = clear_words_collection(words_from_text)
     print(f'words after removing trash = {len(clear_words)}')
-    for _ in clear_words:
-        if len(_) == 4:
-            print(_)
+    word_len = 5
+    add_easy_word_to_ignore(clear_words, word_len)
+
