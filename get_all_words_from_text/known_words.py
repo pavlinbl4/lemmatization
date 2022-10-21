9ad5a4265beed7c7093b0cd15ae76676fa1116fc
evgeniy <pavlinb4@gmail.com>
2022-10-21 10:47:03 +0300

    it will add to clip only unknown words

--- get_all_words_from_text/known_words.py
+++ get_all_words_from_text/known_words.py
@@ -7,8 +7,8 @@ from get_all_words_from_text.use_txt_file import add_word_to_txt
 
 def make_choice(cleared_words,random_index):
     word = cleared_words[random_index]
-    os.system('clear')
-    pyperclip.copy(word)
+    os.system('clear') # clear display in terminal
+    # pyperclip.copy(word)  # save to clip only unknown words
     choice = input(
         f"\nIs word {Fore.GREEN}{word}{Fore.RESET} easy or you want ignore it?\n{Fore.RED}Press Y{Fore.RESET} or "
         f"{Fore.RED}Press N{Fore.RESET}\n")
@@ -21,6 +21,7 @@ def make_choice(cleared_words,random_index):
 
 
     elif choice.lower() == 'n':
+        pyperclip.copy(word)  # save to clip only unknown words
         os.system('clear')
         print(f'{Fore.GREEN} Lets go to next word{Fore.RESET}')
 
