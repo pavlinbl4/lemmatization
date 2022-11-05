388f4bff7ee61d4a0fc5d7b7cf290e2acdf3e4f6
evgeniy <pavlinbl4@gmail.com>
2022-11-05 23:08:19 +0300

    turn off terminal clearing

--- get_all_words_from_text/known_words.py
+++ get_all_words_from_text/known_words.py
@@ -7,8 +7,8 @@ from get_all_words_from_text.use_txt_file import add_word_to_txt
 
 def make_choice(cleared_words,random_index):
     word = cleared_words[random_index]
-    os.system('clear') # clear display in terminal
-    # pyperclip.copy(word)  # save to clip only unknown words
+    # os.system('clear')
+    print(f'{Fore.GREEN} Lets go to next word{Fore.RESET}')
     choice = input(
         f"\nIs word {Fore.GREEN}{word}{Fore.RESET} easy or you want ignore it?\n{Fore.RED}Press Y{Fore.RESET} or "
         f"{Fore.RED}Press N{Fore.RESET}\n")
@@ -22,8 +22,9 @@ def make_choice(cleared_words,random_index):
 
     elif choice.lower() == 'n':
         pyperclip.copy(word)  # save to clip only unknown words
-        os.system('clear')
+        # os.system('clear')
         print(f'{Fore.GREEN} Lets go to next word{Fore.RESET}')
+
 
 
 
