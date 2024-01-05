372381503ca15d295ebd349964388f726170bb8d
evgenii <pavlinbl4@gmail.com>
2024-01-05 20:01:24 +0300

    refactoring

--- get_all_words_from_text/clear_list_of_words.py
+++ get_all_words_from_text/clear_list_of_words.py
@@ -1,9 +1,6 @@
-from get_all_words_from_text.use_txt_file import read_words_file
-
-
-def remove_easy_words(input_list:list,easy_words:list):
-    cleared_words = []
+def remove_easy_words(input_list: list, easy_words: list):
+    cleared_words_list = []
     for word in input_list:
         if len(word) > 2 and word not in easy_words:
-            cleared_words.append(word)
-    return cleared_words
+            cleared_words_list.append(word)
+    return cleared_words_list
