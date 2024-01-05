2660ae633fc337d50b644e2163935adba7083b0a
evgenii <pavlinbl4@gmail.com>
2024-01-05 20:00:15 +0300

    refactoring

--- get_all_words_from_text/use_txt_file.py
+++ get_all_words_from_text/use_txt_file.py
@@ -15,13 +15,13 @@ def add_word_to_txt(input_data, path_to_txt: str):
     with open(path_to_txt, 'a') as text_file:
         text_file.write(f'{input_data}\n')
 
-def write_list_and_replace(input_list:list, path_to_txt):
-    with open(path_to_txt,'w') as over_writen_txt:
+
+def write_list_and_replace(input_list: list, path_to_txt):
+    with open(path_to_txt, 'w') as over_writen_txt:
         for _ in input_list:
             over_writen_txt.write(f'{_.strip()}\n')
 
 
 if __name__ == '__main__':
     list_from_txt = read_words_file('easy_words copy.txt')
-    write_list_and_replace(list_from_txt,'easy_words copy_to_delete.txt')
-
+    write_list_and_replace(list_from_txt, 'easy_words copy_to_delete.txt')
