2df1ad211196dc4896aee72269d47d63cbc30824
evgenii <pavlinbl4@gmail.com>
2024-01-05 20:04:44 +0300

    refactoring

--- get_all_words_from_text/remove_double_words_from_txt_file.py
+++ get_all_words_from_text/remove_double_words_from_txt_file.py
@@ -5,23 +5,23 @@ from pathlib import Path
 from get_all_words_from_text.use_txt_file import read_words_file, write_list_and_replace
 
 
-def main(path_to_file):
+def main_remove_doubles_in_easy_word(path_to_file):
     # list of words from words_file
     durty_list = read_words_file(path_to_file)
-    print(len(durty_list))
+    print(f'known words before optimisation - {len(durty_list)}')
 
     # create set from list
     words_set = set(durty_list)
-    print(len(words_set))
+    print(f"easy words after optimissation - {len(words_set)}")
 
     cleared_words_list = [word for word in words_set if len(word) > 2]
 
     write_list_and_replace(cleared_words_list, path_to_file)
 
     # check result
-    durty_list = read_words_file(path_to_file)
-    print(len(durty_list))
+    # durty_list = read_words_file(path_to_file)
+    # print(len(durty_list))
 
 
 if __name__ == '__main__':
-    print(main(f'{Path().home()}/Documents/ANKI/TEXTS/easy_words.txt'))
+    print(main_remove_doubles_in_easy_word(f'{Path().home()}/Documents/ANKI/TEXTS/easy_words.txt'))
