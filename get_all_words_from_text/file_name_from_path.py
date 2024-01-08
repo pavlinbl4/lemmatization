3b76c337a745a20ea8503175c7b3b2f44a842995
evgenii <pavlinbl4@gmail.com>
2024-01-08 13:06:36 +0300

    get file name from path with extension or not

--- get_all_words_from_text/file_name_from_path.py
+++ get_all_words_from_text/file_name_from_path.py
@@ -5,7 +5,12 @@ def file_name_with_ext(path_to_file: str):
     return Path(path_to_file).name
 
 
+def file_name_no_ext(path_to_file: str):
+    return Path(path_to_file).name.split('.')[0]
+
+
 if __name__ == '__main__':
     path_to_book_file = '/Volumes/big4photo/Downloads/Lamb to the Slaughter.txt'
     assert type(file_name_with_ext(path_to_book_file)) == str
     assert file_name_with_ext(path_to_book_file) == 'Lamb to the Slaughter.txt'
+    print(file_name_no_ext(path_to_book_file))
