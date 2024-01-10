ad36de911aa43ea874de7d84164fa14914190fb1
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-01-10 22:52:31 +0300

    path to easy words in iCloud

--- get_all_words_from_text/new_spacy_words_extractor.py
+++ get_all_words_from_text/new_spacy_words_extractor.py
@@ -28,7 +28,8 @@ def new_spacy_lemma():
     ic(path_to_book_file)
 
     # check existing of the file easy_words.txt and create it if NO
-    easy_words_path_to_txt = f'{Path().home()}/Documents/ANKI/TEXTS/easy_words.txt'
+    # easy_words_path_to_txt = f'{Path().home()}/Documents/ANKI/TEXTS/easy_words.txt'
+    easy_words_path_to_txt = f'{Path().home()}/Library/Mobile Documents/com~apple~CloudDocs/ANKI/TEXTS/easy_words.txt'
     create_file_if_no(easy_words_path_to_txt)
 
     # read text file
@@ -36,7 +37,7 @@ def new_spacy_lemma():
         text = txt.read()
 
     # create nlp doc file and save it to pickle file, if pickle file was created early - read data from pickle
-    doc = nlp_doc(book_title, text, language='en')
+    doc = nlp_doc(book_title, text, language='ru')
 
     # create ent set
     ent_set = set([ent.text for ent in doc.ents])
@@ -60,9 +61,12 @@ def new_spacy_lemma():
     cleared_words_list = set(words)
     # ic(Counter(optimised_words))
 
-    for token in doc:
-        if token.lemma_ in cleared_words_list:
-            print(f'{token.lemma_}\n{token.sent}\n')
+    # for token in doc:
+    #     if token.lemma_ in cleared_words_list:
+    #         print(f'{token.lemma_}\n{token.sent}\n')
+
+    for token in doc.ents:
+        print(token)
 
     # save lemma words to file
     path_to_words_file = create_words_to_learn_or_skip(book_title, words)
@@ -78,3 +82,4 @@ if __name__ == '__main__':
     new_spacy_lemma()
     # get_sentence_from_text('/Users/evgenii/Documents/ANKI/TEXTS/Dolan\'s Cadillac.txt')
     # get_sentence_from_text('/Users/evgenii/Documents/ANKI/TEXTS/Full Dark, No Stars - FAIR EXTENSION.txt')
+    # /Users/evgeniy/Library/Mobile Documents/com~apple~CloudDocs/ANKI
