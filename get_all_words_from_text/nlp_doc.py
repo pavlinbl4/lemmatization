071a66d7ba4c98f7e72408d0f696322d635e84ad
evgeniy <pavlinbl4@gmail.com>
2024-01-12 17:49:04 +0300

    added language detection modul

--- get_all_words_from_text/nlp_doc.py
+++ get_all_words_from_text/nlp_doc.py
@@ -3,8 +3,16 @@ import pickle
 import spacy
 import os
 
+from get_all_words_from_text.language_detection import detect_text_language
 
-def nlp_doc(book_title: str, text: str, language:str):
+
+# python -m spacy download ru_core_news_lg
+# python -m spacy download en_core_web_trf
+# python -m spacy download en_core_web_sm
+# python -m spacy download ru_core_news_sm
+
+
+def nlp_doc(book_title: str, text: str):
     # create sub folder to additional files
     os.makedirs(f'{Path().home()}/Documents/ANKI/TEXTS/{book_title}/', exist_ok=True)
 
@@ -17,11 +25,15 @@ def nlp_doc(book_title: str, text: str, language:str):
 
     else:
         # create doc container
-        assert isinstance(language, object)
+        # assert isinstance(language, object)
+        language = detect_text_language(text)
+
         if language == 'en':
-            nlp = spacy.load("en_core_web_sm")
-        elif language == 'ru':
+            nlp = spacy.load("en_core_web_trf")
+            print("English")
+        else:
             nlp = spacy.load("ru_core_news_lg")
+            print("Russian")
 
 
         doc = nlp(text)
@@ -33,4 +45,4 @@ def nlp_doc(book_title: str, text: str, language:str):
 
 
 if __name__ == '__main__':
-    nlp_doc('Book Title', 'Simple text for test')
+    nlp_doc('Русская книга', 'Крокодилы летят на север')
