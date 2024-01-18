53de80d90fa6a2ea3b18b75551ecd701016bde5d
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-01-18 13:47:43 +0300

    completely modification with using sets and random.simple

--- get_all_words_from_text/known_words.py
+++ get_all_words_from_text/known_words.py
@@ -1,64 +1,80 @@
 # script to find known words in words file and remove them to easy words file
 
+from loguru import logger
 
 import random
 import pyperclip
 from colorama import Fore
-from icecream import ic
+from get_all_words_from_text.use_txt_file import read_words_file
 
-from get_all_words_from_text.clear_list_of_words import remove_easy_words
-from get_all_words_from_text.remove_double_words_from_txt_file import main_remove_doubles_words_in_txt_file
-from get_all_words_from_text.use_txt_file import add_word_to_txt, read_words_file, write_list_and_replace
-from pathlib import Path
+logger.disable(__name__)
 
 
-def make_choice(cleared_words, random_index):
-    word = cleared_words[random_index]
-    choice = input(
-        f"\nIs word {Fore.GREEN}{word}{Fore.RESET} easy or you want ignore it?\n{Fore.RED}Press Y{Fore.RESET} or "
-        f"{Fore.RED}Press N{Fore.RESET}\n")
-
-    while choice.lower() != 'y' and choice.lower() != 'n':
-        choice = input(f"{Fore.GREEN}Please print{Fore.RESET}\n{Fore.RED}Y - to continue{Fore.RESET}\n"
-                       f"{Fore.RED}N - to stop{Fore.RESET}\n")
-
-    # function that add easy word to txt file
-    if choice.lower() == 'y':
-        add_word_to_txt(word, f'{Path().home()}/Documents/ANKI/TEXTS/easy_words.txt')
-
-    elif choice.lower() == 'n':
-        pyperclip.copy(word)  # save to clip only unknown words
-        # os.system('clear')
-        print(f'{Fore.GREEN} Lets go to next word{Fore.RESET}')
+def write_words_from_set(file_path: str, keywords: set):
+    with open(file_path, 'w') as text_file:
+        for k_word in keywords:
+            text_file.write(k_word + '\n')
 
 
-def some_random_words(cleared_words, value_to_test):  # test "value_to_test" words from list - are you know them?
-    used_indexes = []
-    counter = 1
-    while counter <= value_to_test:
-        random_index = random.randint(0, len(cleared_words) - 1)
-        if random_index not in used_indexes:
-            used_indexes.append(random_index)
-            counter += 1
-            make_choice(cleared_words, random_index)
+def make_choice(cleared_words: list, easy_words: set, value_to_test):
+    logger.info(len(easy_words))
+    # index for random words
+    random_digits = random.sample(range(1, len(cleared_words)), value_to_test)
+
+    for index in random_digits:
+
+        word = cleared_words[index]
+        choice = input(
+            f"\nIs word {Fore.GREEN}{word}{Fore.RESET} easy or you want ignore it?\n{Fore.RED}Press Y{Fore.RESET} or "
+            f"{Fore.RED}Press N{Fore.RESET}\n")
+
+        while choice.lower() != 'y' and choice.lower() != 'n':
+            choice = input(f"{Fore.GREEN}Please print{Fore.RESET}\n{Fore.RED}Y - to continue{Fore.RESET}\n"
+                           f"{Fore.RED}N - to stop{Fore.RESET}\n")
+
+        # function that add easy word to set
+        if choice.lower() == 'y':
+            logger.debug(len(easy_words))
+            easy_words.add(word)
+            logger.debug(word)
+            logger.debug(easy_words)
+            logger.debug(len(easy_words))
+
+        elif choice.lower() == 'n':
+            pyperclip.copy(word)  # save to clip only unknown words
+            # os.system('clear')
+            print(f'{Fore.GREEN} Lets go to next word{Fore.RESET}')
+
+    return easy_words
 
 
 if __name__ == '__main__':
-    all_words = '/Users/evgenii/Documents/ANKI/TEXTS/Full Dark, No Stars - FAIR EXTENSION/words_to_learn_file-Full Dark, No Stars - FAIR EXTENSION.txt'
-    easy_words_path = f'{Path().home()}/Documents/ANKI/TEXTS/easy_words.txt'
-    words_from_file = read_words_file(all_words)
-    ic(len(words_from_file))
-    some_random_words(words_from_file, 100)
+    # logger.add('logs/log.log')
+    logger.disable('logs/log.log')
 
-    # new easy words added to test file, no good idea to optimise it
-    main_remove_doubles_words_in_txt_file(easy_words_path)
+    all_words_path = '/Users/evgeniy/Documents/ANKI/TEXTS/words_from_all_file.txt'
+    easy_words_path = '/Users/evgeniy/Library/Mobile Documents/com~apple~CloudDocs/ANKI/TEXTS/easy_words.txt'
 
-    # now remove easy words from words file
-    # remove easy words from file
-    easy_words = read_words_file(easy_words_path)
+    words_from_file: set = set(read_words_file(all_words_path))
+    easy_words: set = set(read_words_file(easy_words_path))
 
-    cleared_words_list = remove_easy_words(words_from_file, easy_words)
-    ic(len(cleared_words_list))
+    logger.info(len(words_from_file))
+    logger.info(len(easy_words))
+    logger.info(easy_words)
 
-    # write_list_and_replace(cleared_words_list, all_words)
-    write_list_and_replace(cleared_words_list, all_words)
+    words_from_file: set = words_from_file - easy_words
+    logger.info(len(words_from_file))
+
+    # updated easy_words set
+    easy_words = make_choice(list(words_from_file), easy_words, value_to_test=3)
+    logger.info(len(easy_words))
+
+    # clear words from file again
+    cleared_words_from_file: set = words_from_file - easy_words
+    logger.info(len(cleared_words_from_file))
+    logger.info(cleared_words_from_file)
+    logger.info(easy_words)
+
+    # write sets to text files
+    write_words_from_set(all_words_path, words_from_file)
+    write_words_from_set(easy_words_path, easy_words)
